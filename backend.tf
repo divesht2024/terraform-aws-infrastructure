d6c1d904ee1d440ae46e91be23cb586acc3c5d48
Divesh Tayade <162043849+divesht2024@users.noreply.github.com>
2026-05-18 10:24:46 +0530

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -1,1 +1,9 @@
-
+terraform {
+  backend "s3" {
+    bucket         = "my-terraform-state-divesh"
+    key            = "dev/terraform.tfstate"
+    region         = "ap-south-1"
+    dynamodb_table = "terraform-lock"
+    encrypt        = true
+  }
+}
