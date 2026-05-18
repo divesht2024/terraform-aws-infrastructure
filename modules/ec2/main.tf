9c7a1754f578232c1bd6e9270e178f8314eeeee1
Divesh Tayade <162043849+divesht2024@users.noreply.github.com>
2026-05-18 10:21:19 +0530

    Update main.tf

--- modules/ec2/main.tf
+++ modules/ec2/main.tf
@@ -1,1 +1,12 @@
+resource "aws_instance" "server" {
+  ami                    = var.ami
+  instance_type          = "t2.nano"
+  subnet_id              = var.subnet_id
+  vpc_security_group_ids = [var.sg_id]
+  iam_instance_profile   = var.profile_name
+  associate_public_ip_address = true
 
+  tags = {
+    Name = "Terraform-Server"
+  }
+}
